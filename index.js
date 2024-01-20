66018d263144df46da88b79200ef94e92d1cff5f
Soumyaranjan panda <91568463+soummyaanon@users.noreply.github.com>
2024-01-20 14:34:44 +0530

    Please enter the commit message for your changes. Lines starting

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require("express");
-const app = express();
+const express =require("express")
+const app =express();
 const User = require("./database");
+
 
 
 app.set("view engine","ejs");
@@ -15,48 +16,50 @@ app.get("/",async(req,res)=>{
   
 })
 
-app.get("/register", (req, res) => {
-  res.render("register");
+app.post("/register",async(req,res)=>{
+    const {name,email,password}=req.body;
+    const newuser = new User({name,email,password});
+    const usersave = await newuser.save();
+    res.redirect("/");
 })
 
-app.get("/edit/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const user = await User.findById(id);
-    if (!user) {
-      res.redirect("/");
-    } else {
-      res.render("edit", { user });
+app.get("/register",(req,res)=>{
+    res.render("register");
+})
+
+app.get("/edit/:id",async(req,res)=>{
+    const {id} =req.params;
+    const user = await User.findById({_id:id});
+    if(user==null){
+        res.redirect("/");
+    }else{
+        res.render("edit",{
+            user:user
+        })
     }
-  } catch (error) {
-    console.error(error);
-    res.redirect("/");
-  }
 })
 
-app.post("/update/:id", async (req, res) => {
-  const { id } = req.params;
-  const { name, email, password } = req.body;
-  try {
-    const updatedUser = await User.findByIdAndUpdate(id, { name, email, password }, { new: true });
+app.post("/update/:id",async(req,res)=>{
+    const {id} =req.params;
+    const {name,email,password}=req.body;
+    const updateuser = await User.findByIdAndUpdate({_id:id},
+        {name,email,password},
+        {new:true})
     res.redirect("/");
-  } catch (error) {
-    console.error(error);
-    res.redirect("/");
-  }
 })
 
-app.get("/delete/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const deletedUser = await User.findByIdAndDelete(id);
-    res.redirect("/");
-  } catch (error) {
-    console.error(error);
-    res.redirect("/");
-  }
+app.get("/delete/:id",async(req,res)=>{
+    const {id} =req.params;
+const deleteuser =await User.findByIdAndDelete({_id:id});
+res.redirect("/");
 })
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+
+
+
+
+
+
+app.listen(5000,()=>{
+    console.log("server listeninig on portno:5000");
 })
